9ad28497589dd86a753cfd4b183fcb629e5e73e7
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-08 22:59:32 +0300

    Lecture_7_Func2_Locking_DataTypes_Cycles

--- js/index.js
+++ js/index.js
@@ -1,279 +1,128 @@
-// Варіанти створення об'єктів
-let obj = {
-  apple: 3,
-  bananas: 34,
-  qiwi: 45,
-};
-
-// достуа до властивостей
-console.log(obj); //Подивитись, що в об'єкті
-console.log(obj.apple); //Подивитись значення, написати ключ
-
-// Збереження об'єктів у пам'яті
-obj.apple = 45; //зміна значення ключа
-console.log(obj.apple);
-
-// Додавання в об'єкт
-obj.orange = 23;
-console.log(obj);
-
-// Object.freeze()
-Object.freeze(obj); //Заморожування об'кта, після чого його не можна змінити
-obj.apple = 70;
-console.log(obj.apple); // 45
-
-// видалення (не працює з freeze та seal)
-let animal = {
-  rabbit: 3,
-  turtle: 5,
-  cat: 7,
-};
-console.log(animal);
-
-// Object.seal()
-Object.seal(animal); // не дає видаляти, але дозволяє змінювати
-delete animal.cat;
-animal.turtle = 8;
-console.log(animal);
-
-// КОПІЮВАННЯ об'єктів
-
-// const animalCopy = animal; // !проблема: змінюється основний масив
-// animalCopy.turtle = 11;
-// console.log(animal);
-
-// правильне копіювання
-
-// 1 копіювання першого рівня вкладеності
-let fruits = {
-  apple: 3,
-  bananas: 34,
-  qiwi: 45,
-};
-const fruitsCopy = {}; //спочатку створюємо масив куди хочемо скопіювати
-Object.assign(fruitsCopy, fruits); // першим параметром вказуємо об'єкти куди копіюємо, другий параметр це об'єкт з якого робиться копія
-fruits.bananas = 12;
-console.log(fruitsCopy); // зміни у fruits не вплинули на fruitsCopy
-fruitsCopy.bananas = 25;
-console.log(fruits);
-console.log(fruitsCopy);
-
-// 2 глибоке копіювання
-let animalPeople = {
-  rabbit: 3,
-  turtle: 5,
-  user: {
-    name: 'Ihor',
-    surname: 'Mykh',
+// From lecture 6
+// DEEP COPY by ASSIGN
+const user = {
+  name: 'Ihor',
+  surname: 'Mykh',
+  date: {
+    id: 23,
+    age: 25,
   },
-  cat: 7,
 };
 
-// Object.keys()
-console.log(Object.keys(animalPeople)); // виводе масив ключів ['rabbit', 'turtle', 'user', 'cat']
-
-// for ... in по ключам
-for (let item in obj) {
-  console.log(item); // виведе вписок ключів obj
-}
-
-// Object.values()
-console.log(Object.values(animalPeople)); // видає значення ключів [3, 5, {…}, 7]
-
-// 1
-// for ... in по значенням
-for (let item in animalPeople) {
-  console.log(animalPeople[item]); // виведе список значень ключів obj
-}
-
-// 2
-// for ... of по значенням об'єкта
-for (let value of Object.values(animalPeople)) {
-  console.log(value); // виведе список значень ключів obj
-}
-
-// for ... of так само може ходити по значенням масиву
-let arr = [1, 2, 3, 4, 5];
-for (let value of arr) {
-  console.log(value); // виведе список значень ключів obj
-}
-
-//  Глибоке клонування приклад від викладача
-// подивитись тип item (кожен раз дивиться тип значення за ключем)
-// (особливо важливе тоді, коли вихідний об'єкт містить вкладені об'єкти або масиви)
-let animalPeopleCopy = {};
-for (let item in obj) {
-  if (typeof obj[item] === 'object') {
-    console.log('Object');
+const userCopy = {};
+for (let item in user) {
+  if (typeof user[item] === 'object') {
+    userCopy[item] = Object.assign({}, user[item]);
   } else {
-    animalPeopleCopy[item] = obj[item];
-  }
-
-  console.log(animalPeopleCopy);
-}
-
-// приклад з chatGPT
-function deepClone(obj) {
-  if (obj === null || typeof obj !== 'object') {
-    return obj; // повертаємо примітиви і null без змін
-  }
-
-  if (Array.isArray(obj)) {
-    // якщо це масив, клонуємо його елементи
-    return obj.map(deepClone);
-  }
-
-  // якщо це об'єкт, клонуємо його властивості
-  const clonedObj = {};
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      clonedObj[key] = deepClone(obj[key]);
-    }
-  }
-
-  return clonedObj;
-}
-
-// let animalPeople вже була об'явлена вище, тому закоментила
-// let animalPeople = {
-//   rabbit: 3,
-//   turtle: 5,
-//   user: {
-//     name: 'Ihor',
-//     surname: 'Mykh',
-//   },
-//   cat: 7,
-// };
-
-let animalPeopleCopy2 = deepClone(animalPeople);
-console.log(animalPeopleCopy2);
-
-// JSON
-// переводе об'єкт у строку
-let trees = {
-  birch: 5,
-  oak: 2,
-  bush: {
-    raspberry: 3,
-    currant: 2,
-  },
-  maple: 7,
-};
-// JSON.stringify переводить об'єкт у строку
-console.log(JSON.stringify(trees)); // {"birch":5,"oak":2,"bush":{"raspberry":3,"currant":2},"maple":7}
-
-// JSON.parse виводить з формату строки зворотньо у об'єкт
-console.log(JSON.parse(JSON.stringify(trees))); // {birch: 5, oak: 2, bush: {…}, maple: 7}
-
-let treesCopy = JSON.parse(JSON.stringify(trees));
-trees.bush.currant = 'Alex';
-console.log(treesCopy); //не змінений, бо відбулося глибоке клонування
-
-//
-// ФУНКЦІЇ Основи роботи
-//
-// Spread operator ...
-let arr1 = [1, 2, 3, 4];
-let arrCopy = [...arr1];
-console.log(arrCopy); //copy array
-
-const fruit2 = {
-  apple: 4,
-  banana: 5,
-};
-let fruit2Copy = { ...fruit2 };
-console.log(fruit2Copy); // copy object (the shape of the parentheses matters)
-
-// function declaration
-function func() {
-  console.log('1');
-}
-
-// function expression
-const func1 = function func() {
-  console.log('2');
-};
-
-// Передача параметрів у функцію
-function multNumByTwo(number, multBy = 2) {
-  return number * multBy;
-}
-console.log(multNumByTwo(4, 4));
-
-// якщо є два параметри, але другий не обов'язковий і треба щоб функція працювали і з одним
-function SumName(name, surname = '') {
-  return name + surname;
-}
-console.log(SumName('John'));
-
-// якщо не знаємо кількість аргументів (операторів)
-function spreadOperator(...args) {
-  console.log(args);
-}
-console.log(spreadOperator('John', 'Smith', 'Jo')); //['John', 'Smith', 'Jo']
-
-// перебір масива
-function searchArray(...args) {
-  for (let i = 0; i < args.length; i++) {
-    console.log(args[i]);
+    userCopy[item] = user[item];
   }
 }
-console.log(searchArray('John', 'Smith', 'Jo'));
-// John
-// Smith
-// Jo
 
-// стрілочни функції
-let fff = () => {};
+user.date.id = 4;
+console.log(userCopy);
+
+// Lecture 7
+// RECURSION
+// Факторіал 5! = 1 * 2 * 3 * 4 * 5
+console.log(func(5));
+
+function func(number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * func(number - 1);
+  }
+}
+
+// ФУНКЦІЯ, ЩО ЗАМИКАЄТЬЧЯ IIFE (Immediately Invoked Function Expression)
+// option 1
+(function func1() {
+  console.log(1);
+})();
+
+// option 2
+let x = (function func2() {
+  return 2;
+})();
+console.log(x);
+
+// // ЗАМИКАННЯ
+function countManipulation() {
+  let count = 0; // Заводимо лічильник
+
+  // нам треба кожен раз, коли ми викликаємо функцію countIncrement збільшувати лічильник
+  function countIncrement() {
+    count++;
+    console.log(count);
+  }
+  return countIncrement;
+}
+
+let newFunc = countManipulation();
+newFunc();
+newFunc();
+newFunc();
+newFunc();
+
+// Math. ...
+// отримання рандомного числа
+console.log(Math.random()); // поверне рандомне число від 0 до 1
+console.log(Math.random() * 10); // поверне рандомне число від 0 до 10
+
+let num = 123.623672;
+console.log(Math.round(num)); //округляє число до більшого числа
+console.log(Math.floor(num)); // відсікає дрібну частину
+
+// toFixed округлює до вказаної кількості знаків після коми за правилами математики
+console.log(num.toFixed(4));
+
+// string
+// split, join
+let name = 'vdyicvgewjndcio';
+console.log(name[4]); // виводить конкретий елемент за індексом
+console.log(name.split('')); // розбиває рядок на масив рядків, шляхом поділу зазначеним підрядком('', ' ', або будь який інший знак)
+console.log(name.split('').join('')); // поєднує всі елементи масиву у рядок (спочатку поділили, а потім об'єєднали)
+console.log(name.split('').join(',')); // поєднує всі елементи масиву у рядок, але виведе кожен елемент через кому
+
+let str = 'vdyicvgewjndcio';
+
+let arr = str.split(''); // розбиває рядок на масив рядків, шляхом поділу зазначеним підрядком
+console.log(arr);
+
+let newString = arr.join(','); // поєднує всі елементи масиву у рядок, але виведе кожен елемент через кому
+console.log(newString);
+
+// replace, parseInt, parseFloat (привести рядок с зайвими знаками до чистого числа)
+let cost = '$156778,365';
+console.log(cost.replace('$', '')); // заміщує якись елементи в рядку на вказані, перший аргумент -  це, що замінити, другий - на що
+console.log(cost.replace(',', '.')); // заміна коми на крапку
+let costWhithoutSymbols = cost.replace('$', '').replace(',', '.'); // об'єднаний варіант
+console.log(parseInt(costWhithoutSymbols)); //приводе рядок у число, але тільки у ціле число відкидаючи дробову частину
+console.log(parseFloat(costWhithoutSymbols)); //приводе рядок у число, зберігає дробову частину
+
+// slice (повертає новий масив, який копіює елементи, починаючи з індексу start і до end (не включаючи end))
+let costWhithoutSymbols2 = cost.slice(1); // видалить перший знак(знак долара), тобто нульовий індекс, а с першого і далв все виведе
+console.log(costWhithoutSymbols2);
+
+let cost2 = '$156778,365';
+let costWhithoutSymbols3 = cost2.slice(2, 4); // виведе з другого індекса по четвертий
+console.log(costWhithoutSymbols3);
+
+let costWhithoutSymbols4 = cost2.slice(-4); // залишає елементи починаючі з кінця, інші видаляє
+console.log(costWhithoutSymbols4);
 
 // HW
-// Дан масив об'єктів. Вивести масив телефонних номерів користувачів, у яких баланс більше 2000 доларів. І знайти суму всіх балансів користувачів
-// переробити через forEach
-let users = [
-  {
-    index: 0,
-    isActive: true,
-    balance: 2226.6,
-    name: 'Eugenia Sawyer',
-    gender: 'female',
-    phone: '+1 (840) 583-3207',
-    address: '949 John Street, Rose, Puerto Rico, 1857',
-  },
-  {
-    index: 1,
-    isActive: true,
-    balance: 2613.77,
-    name: 'Pauline Gallegos',
-    gender: 'female',
-    phone: '+1 (985) 593-3328',
-    address: '328 Greenpoint Avenue, Torboy, North Dakota, 6857',
-  },
-  {
-    index: 5,
-    isActive: false,
-    balance: 1790.56,
-    name: 'Suzette Lewis',
-    gender: 'female',
-    phone: '+1 (837) 586-3283',
-    address: '314 Dunne Place, Bawcomville, Guam, 9053',
-  },
-];
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-let arrResult = [];
-for (let i = 0; i < users.length; i++) {
-  if (users[i].balance > 2000) {
-    arrResult.push(users[i]);
-  }
+function generateKey(length, characters) {
+  let key = '';
+  let index = Math.round(Math.random() * 10); // округлюємо та додаємо до key випадковий символ з надоного набору key (16 разів)
+  console.log(index); // номер індекса з characters
+  // додати цикл
+  key += characters[index]; // перебираємо доки не буде 16 index в key
+
+  return key;
 }
-console.log(arrResult);
 
-// Перероблено через forEach
-let arrResult = [];
-
-users.forEach((user) => {
-  if (user.balance > 2000) {
-    arrResult.push(user);
-  } // user.balance > 2000 ? arrResult.push(user) : null;
-});
-
-console.log(arrResult);
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
