bc6690bb14adec54c3281e01247a8f1ac40030fd
Tetiana Peleshko <t.peleshko@gmail.com>
2023-11-05 13:16:02 +0200

    Lecture_19_Modules_NPM_NodeJs_Part2

---
+++ js/mathNode.js
@@ -0,0 +1,10 @@
+function addNumbers(a, b) {
+  console.log(a + b);
+}
+
+function multiplyNumbers(a, b) {
+  console.log(a * b);
+}
+
+module.exports.addNumbers = addNumbers;
+module.exports.multiplyNumbers = multiplyNumbers;
---
+++ js/mathUtils.js
@@ -0,0 +1,12 @@
+export function myOwnAdd(a, b) {
+  return a + b;
+}
+
+export function myOwnMultiply(a, b) {
+  return a * b;
+}
+
+// export default може бути лише один на файл
+export default function myOwnAdd1(a, b) {
+  return a + b;
+}
